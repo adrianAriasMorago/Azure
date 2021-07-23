018fc626c6470c9edacc41053fcedb7f7be43340
adrianAriasMorago <83167546+adrianAriasMorago@users.noreply.github.com>
2021-07-23 15:12:05 +0200

    Add files via upload

---
+++ terraform/credentials.tf
@@ -0,0 +1,9 @@
+#
+# provider "azurerm" {
+#  features {}
+#  subscription_id = "927d0301-3031-467c-9b95-a3d0135304a7"
+#  client_id       = "6d28fbbe-54d5-4540-9241-b218ff1a9a59"
+#  client_secret   = "e~tD~rjfHWELBfhBKkC1hMCAtNbPNbKAfD"
+#  tenant_id       = "899789dc-202f-44b4-8472-a6d40f9eb440"
+# }
+#
